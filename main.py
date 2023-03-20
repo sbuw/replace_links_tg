e81d6da8f2941b411487f073d0c82bff16cd1af8
fmd <83774679+sbuw@users.noreply.github.com>
2023-03-20 22:18:03 +0500

    add files

---
+++ main.py
@@ -0,0 +1,46 @@
+# ----------------------------------------
+
+# WORKS ONLY FROM USER ACCOUNTS, THROUGH THE BOTS DOES NOT WORK
+# WORKS ONLY FROM USER ACCOUNTS, THROUGH THE BOTS DOES NOT WORK
+# WORKS ONLY FROM USER ACCOUNTS, THROUGH THE BOTS DOES NOT WORK
+
+# ----------------------------------------
+
+# ADS
+# Channel about news from the front and the dark world --> t.me/ZPvoenkor
+# Channel about news from the front and the dark world --> t.me/ZPvoenkor
+# Channel about news from the front and the dark world --> t.me/ZPvoenkor
+# ADS
+
+# ----------------------------------------
+
+from pyrogram import Client, enums
+from time import sleep
+
+api_id = 0123456 # my.telegram.org/
+api_hash = "123345adfg2345" # my.telegram.org/
+
+m_chat_id = -1234567890
+
+old_link = ""
+new_link = ""
+
+app = Client("bot1", api_id=api_id, api_hash=api_hash)
+app.start()
+
+# entities
+# caption_entities
+
+for msg in app.get_chat_history(m_chat_id):
+	
+	try:
+		if msg.text[-len(old_link):] == old_link : msg.edit_text(msg.text[:-(len(old_link)+1)] + f"**{new_link}**", parse_mode=enums.ParseMode.MARKDOWN)
+	except:
+		pass
+	try:
+		if msg.caption[-8:] == "old link": msg.edit_caption(msg.caption[:-9] + "**new link**", parse_mode=enums.ParseMode.MARKDOWN)
+	except:
+		pass
+	sleep(0.2)
+	
+app.stop()
---
+++ readme.md
@@ -0,0 +1,7 @@
+## • Replace links in Telegram posts
+
+### • Instructions for use
+- Download python
+- Install the dependencies: `pip install pyrogram`
+- Open the file and replace the variables `api_id` `api_hash` `m_chat_id` `old_link` `new_link`
+- Run the tool: `python3 main.py`
